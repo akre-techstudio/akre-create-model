80d32af4ce5ed1d8037e7db856b871a2798a83c3
elsatmr <elsaatamara@gmail.com>
2023-09-26 20:30:27 -0700

    entry script changed

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
   ],
   entry_points={
     'console_scripts': [
-        'akre = akre.create_akre_model:main'
+        'akre = akre.create_akre_model:create_model'
     ]
   },
   classifiers=[
